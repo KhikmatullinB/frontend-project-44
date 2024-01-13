cb41989bd2a5ecf5516b13bacdb3c239807c79d0
Khikmatullin Bulat <colonspawn@gmail.com>
2024-01-13 17:26:51 +0800

    little dirty updates

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,21 +1,33 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
+import greetings from '../src/cli.js';
 
+const userName = greetings();
 const rounds = 3;
 const brainEvenRules = 'Answer "yes" if the number is even, otherwise answer "no".';
 const randomNumber = (min, max) => Math.floor(Math.random() * (max - min)) + min;
-const evenNum = randomNumber % 2 === 0;
-const correctAnswer = evenNum ? 'yes' : 'no';
 console.log(brainEvenRules);
+
 const numQuestion = () => {
-    console.log('Question:' randomNumber(1, 100));
+    const tempNumber = randomNumber(1, 10);
+    console.log('Question: ', tempNumber);
+    return tempNumber;
 };
-const userAnswer = readlineSync.question('Your answer: ');
-for (let i = 0; i <= rounds; i += 1) {
+
+for (let i = 0; i < rounds; i += 1) { 
+    const tempNumber = numQuestion();
+    const correctAnswer = tempNumber % 2 ? 'no' : 'yes';
+    const userAnswer = readlineSync.question('Your answer: ');
+
     if (userAnswer !== correctAnswer) {
         console.log(`'${userAnswer}' is wrong answer. Correct answer was '${correctAnswer}'.`);
-        return console.log(`Let's try again, ${userName}`);
+        console.log(`Let's try again, ${userName}`);
+        return;
+    } 
+    else if (userAnswer === correctAnswer) {
+    console.log('Correct!');
     }
-    return console.log('Correct!');
-    }
-    return console.log(`Congratulations, ${userName}!`)
+}; 
+console.log(`Congratulations, ${userName}!`);
+return;
+
