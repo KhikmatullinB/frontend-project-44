dccedbb44010b9faf4f667f540e34ab7d86a9dab
Khikmatullin Bulat <colonspawn@gmail.com>
2024-01-15 14:02:21 +0800

    finished brain-calc game

--- src/games/cli-calc.js
+++ src/games/cli-calc.js
@@ -1,1 +1,29 @@
 import gameProcess from '../index.js';
+import randomNumber from '../randomNumber.js'
+
+const gameRules = 'What is the result of the expression?';
+const operands = ['+', '-', '*'];
+
+const expression = (num1, operand, num2) => {
+    switch(operand) {
+        case '+':
+            return num1 + num2;       
+        case '-':
+            return num1 - num2;
+        case '*':
+            return num1 * num2;
+        default:
+            null;
+    };
+};
+const generateRounds = () => {
+    const num1 = randomNumber(1, 10);
+    const num2 = randomNumber(1, 10);
+    const randomOperand = operands[randomNumber(0, operands.length -1)];
+    const question = `${num1}${randomOperand}${num2}`;
+    const correctAnswer = `${expression(num1, randomOperand, num2)}`;
+    return [correctAnswer, question];
+};
+const brainCalcGame = () => gameProcess(gameRules, generateRounds);
+
+export default brainCalcGame;
