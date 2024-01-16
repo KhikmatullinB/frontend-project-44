2b57f0e584a169d32d08149706977514a2805657
Khikmatullin Bulat <colonspawn@gmail.com>
2024-01-16 12:05:08 +0800

    last eslint fixes

--- src/games/cli-prime.js
+++ src/games/cli-prime.js
@@ -4,23 +4,23 @@ import gameProcess from '../index.js';
 const gameRules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
-    if (num < 2) {
+  if (num < 2) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) {
       return false;
     }
-    for (let i = 2; i <= Math.sqrt(num); i += 1) {
-      if (num % i === 0) {
-        return false;
-      }
-    }
-    return true;
-  };
-
-  function generateRounds() {
-    const number = randomNumber(1, 10);
-    const question = `${number}`;
-    const correctAnswer = isPrime(number) ? 'yes' : 'no';
-    return [question, correctAnswer];
   }
-  const primeGame = () => gameProcess(gameRules, generateRounds);
-  
-  export default primeGame;
+  return true;
+};
+
+function generateRounds() {
+  const number = randomNumber(1, 10);
+  const question = `${number}`;
+  const correctAnswer = isPrime(number) ? 'yes' : 'no';
+  return [question, correctAnswer];
+}
+const primeGame = () => gameProcess(gameRules, generateRounds);
+
+export default primeGame;
--- src/games/cli-progression.js
+++ src/games/cli-progression.js
@@ -4,25 +4,25 @@ import gameProcess from '../index.js';
 const gameRules = 'What number is missing in the progression?';
 
 const generateProgression = (firstItem, step, Length) => {
-    const array = [];
-    for (let i = 0; i < Length; i += 1) {
-        const result = (firstItem + (step * i));
-        array.push(result);
-    }
-    return array;
+  const array = [];
+  for (let i = 0; i < Length; i += 1) {
+    const result = (firstItem + (step * i));
+    array.push(result);
+  }
+  return array;
 };
 function generateRounds() {
-    const first = randomNumber(1, 10);
-    const step = randomNumber(1, 10);
-    const length = randomNumber(5, 10);
-    const progression = generateProgression(first, step, length);
-    const randIndex = Math.floor(Math.random() * progression.length);
-    const correctAnswer = `${progression[randIndex]}`;
-    progression[randIndex] = '..';
-    const question = progression.join(' ');
-    return [question, correctAnswer];
-  }
-  
-  const progressionGame = () => gameProcess(gameRules, generateRounds);
-  
-  export default progressionGame;
+  const first = randomNumber(1, 10);
+  const step = randomNumber(1, 10);
+  const length = randomNumber(5, 10);
+  const progression = generateProgression(first, step, length);
+  const randIndex = Math.floor(Math.random() * progression.length);
+  const correctAnswer = `${progression[randIndex]}`;
+  progression[randIndex] = '..';
+  const question = progression.join(' ');
+  return [question, correctAnswer];
+}
+
+const progressionGame = () => gameProcess(gameRules, generateRounds);
+
+export default progressionGame;
